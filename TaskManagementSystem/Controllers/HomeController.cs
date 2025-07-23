3518aecf694cbdd72edb99c54e69e790731ed804
epiccipe <brokeprotocol4916@gmail.com>
2025-07-24 01:16:00 +0500

    Note delete feature

--- TaskManagementSystem/Controllers/HomeController.cs
+++ TaskManagementSystem/Controllers/HomeController.cs
@@ -1,19 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using TaskManagementSystem.Models;
 
 namespace TaskManagementSystem.Controllers
 {
     public class HomeController : Controller
     {
-        private NodeContext _notedb;
+        private NodeContext _nodedb;
         public HomeController(NodeContext noteContext)
         {
-            _notedb = noteContext;
+            _nodedb = noteContext;
         }
         [HttpGet]
         public IActionResult Nodes()
         {
-            var nodes = _notedb.Nodes.ToList();
+            var nodes = _nodedb.Nodes.ToList();
 
             return View(nodes);
         }
@@ -25,10 +26,23 @@ namespace TaskManagementSystem.Controllers
         [HttpPost]
         public async Task<IActionResult> AddNode(Node node)
         {
-            await _notedb.Nodes.AddAsync(node);
-            await _notedb.SaveChangesAsync();
+            await _nodedb.Nodes.AddAsync(node);
+            await _nodedb.SaveChangesAsync();
 
             return RedirectToAction("Nodes");
+        }
+        [HttpPost]
+        public async Task<IActionResult> DeleteNode(int id)
+        {
+            Node? node = await _nodedb.Nodes.FirstOrDefaultAsync(n => n.Id == id);
+
+            if (node is null) return RedirectToRoute("Main");
+
+            _nodedb.Nodes.Remove(node);
+
+            await _nodedb.SaveChangesAsync();
+
+            return RedirectToRoute("Main");
         }
     }
 }
